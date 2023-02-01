f7bbd76609f00e0191a32c2c1428ef87a9f6f441
VivianaS14 <vasorianp@gmail.com>
2023-02-01 16:48:17 -0500

    configuracion login

--- src/db.js
+++ src/db.js
@@ -1,16 +1,20 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBr63uW7BsFJYmIxXg70H4VPjq1qeejxjY",
-    authDomain: "quiz-app-7a089.firebaseapp.com",
-    projectId: "quiz-app-7a089",
-    storageBucket: "quiz-app-7a089.appspot.com",
-    messagingSenderId: "631358657789",
-    appId: "1:631358657789:web:6c5e950a63532448c81a94",
-    measurementId: "G-FZZGVH4KW5"
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  apiKey: "AIzaSyBr63uW7BsFJYmIxXg70H4VPjq1qeejxjY",
+  authDomain: "quiz-app-7a089.firebaseapp.com",
+  projectId: "quiz-app-7a089",
+  storageBucket: "quiz-app-7a089.appspot.com",
+  messagingSenderId: "631358657789",
+  appId: "1:631358657789:web:6c5e950a63532448c81a94",
+  measurementId: "G-FZZGVH4KW5"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth()
+
+export { app, auth }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React from "react";
+import { auth } from "../db";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const Login = () => {
+  const onLoginGoogle = async () => {
+    /* app
+      .auth()
+      .signInWithPopup(new GoogleAuthProvider())
+      .then((e) => console.log(e)); */
+    try {
+      const result = await signInWithPopup(auth, new GoogleAuthProvider());
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='contenedor' >
-      <div style={{background:"white", width:"100%",height:"100%"}}>
-        <h1>hola</h1>
-        <h2>caho</h2>
+    <div className="contenedor">
+      <div className="contenedorMobile">
+        <button type="button" onClick={onLoginGoogle}>
+          Continuar con Google
+        </button>
+        <hr />
+        <form action="">
+          <label htmlFor="email">Correo Electronico</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Ingrese su correo electronico"
+          />
+          <label htmlFor="email">Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Ingrese su contraseña"
+          />
+          <button type="submit">Iniciar Sesion</button>
+          <button type="submit">Registrarse</button>
+        </form>
+        <hr />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
