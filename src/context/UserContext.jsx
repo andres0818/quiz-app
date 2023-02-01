2154e565b61e4a9ab1d1dc1e2686d8e7b019d153
andres0818 <c.andres.perez.h@gmail.com>
2023-02-01 17:50:22 -0500

    estado del usuario

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,15 +1,19 @@
 import { createContext, useState } from "react";
 import { auth } from "../db";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [user, setUser] = useState([]);
+  const navigate = useNavigate();
+
 
   const loginGoogle = () => {
     signInWithPopup(auth, new GoogleAuthProvider()).then((result) =>
-      setUser(result.user)
+      setUser(result.user),
+      navigate('/home')
     );
   };
 
