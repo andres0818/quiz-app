d2dfa75592d9c015230d3cf2637d67f43f037a13
andres0818 <c.andres.perez.h@gmail.com>
2023-02-06 08:58:54 -0500

    cambio en la implementacion de las rutas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
